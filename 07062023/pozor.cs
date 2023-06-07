ae0f9e766a7d8a850eb0a971f2d605db5b78cae2
Александр Гурьев <arh-tehnik@mail.ru>
2023-06-07 20:43:08 +0300

    jhdgf

---
+++ 07062023/pozor.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _07062023
+{
+    internal class Human
+    {
+        protected string _name;
+        protected string _Lastname;
+
+        public Human (string n, string l)
+        {
+            _name = n;
+            _Lastname =l;
+
+        }
+    }
+    class bulder : Human
+    {
+        double _salary;
+        int _age;
+        public bulder( string n,string l, int a, double s):base (n,l)
+        {
+            
+            _salary = s;
+            
+            _age= a;
+
+        }
+    }
+    class Sailor : Human
+    {
+        int _insea;
+        public Sailor(string n, string l, int i) : base(n, l)
+        {
+           
+            
+      
+            _insea = i;
+
+        }
+        public void Print()
+        {
+            Console.WriteLine($"Моряк  ={_Lastname}{_name}." + $"Часов в море  = {_insea}");
+        }
+    }
+    class Pilot : Human
+    {
+        int _time_to;
+        public Pilot(string n, string l, int t): base(n, l)
+        {
+            
+            _time_to = t;
+
+        }
+        public void Print()
+        {
+            Console.WriteLine($"Пилот   ={_Lastname}{_name}." + $"Часов налета   = {_time_to}");
+        }
+    }
+}
